d2dcedd0e2e27166cf5b9a5f40c1535ab28ea1f7
ariscampini <75635976+ariscampini@users.noreply.github.com>
2021-03-29 13:18:18 +0200

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <div className="container">
       <h1>Weather App</h1>
       <footer>
         This project was coded by Arianna Scampini and is {" "}
       <a href="https://github.com/ariscampini/react-weatherapp-finalproject" 
       target="_blank">open source on GitHub</a>
       </footer>
+      </div>
     </div>
   );
 }
