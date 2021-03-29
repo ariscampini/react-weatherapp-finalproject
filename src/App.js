4b439a7534cb49b8b47e9d6e9316ab47ec8a2743
ariscampini <75635976+ariscampini@users.noreply.github.com>
2021-03-29 13:07:26 +0200

    Added footer

--- src/App.js
+++ src/App.js
@@ -3,7 +3,12 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by Arianna Scampini and is {" "}
+      <a href="https://github.com/ariscampini/react-weatherapp-finalproject" 
+      target="_blank">open source on GitHub</a>
+      </footer>
     </div>
   );
 }
