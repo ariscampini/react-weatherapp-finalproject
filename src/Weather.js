81d02da45a81f14884d76241a70192c0f2dd8f95
ariscampini <75635976+ariscampini@users.noreply.github.com>
2021-04-03 17:47:44 +0200

    Deleted the entire const

--- src/Weather.js
+++ src/Weather.js
@@ -7,7 +7,6 @@ import "./Weather.css";
 export default function Weather (props){
 
     const [cityName, setCityName] = useState(props.defaultCityName);
-    const [setReady] = useState(false);
     const [weatherData, setWeatherData] = useState ({ready:false});
 
     function handleResponse (response){
@@ -25,7 +24,6 @@ export default function Weather (props){
             icon: response.data.weather[0].icon,
             description: response.data.weather[0].description,
         });
-        setReady(true);
     }
 
 function search() {
